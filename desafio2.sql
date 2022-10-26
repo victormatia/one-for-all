44171a2bfb2f4f784c93325901851345d63abefc
Victor Matias <fvictor1705@gmail.com>
2022-10-26 11:17:33 -0300

    feat: solves requirement 2

--- desafio2.sql
+++ desafio2.sql
@@ -1,1 +1,8 @@
--- initial commit
+SELECT COUNT(DISTINCT t.track_id) AS cancoes,
+COUNT(DISTINCT a.artist_id) AS artistas,
+COUNT(DISTINCT al.album_id) AS albuns
+FROM SpotifyClone.tracks AS t
+INNER JOIN SpotifyClone.artists AS a
+INNER JOIN SpotifyClone.albums AS al
+ON al.album_id = t.album_id
+AND a.artist_id = al.artist_id;
