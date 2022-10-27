e2b7f135c137d7628982ce32eade004fb7d0765d
Victor Matias <fvictor1705@gmail.com>
2022-10-27 00:29:33 -0300

    feat: solves requirement 10

--- desafio10.sql
+++ desafio10.sql
@@ -1,3 +1,4 @@
+-- Active: 1666721960658@@127.0.0.1@3307@SpotifyClone
 SELECT
   t.track_name AS nome,
   COUNT(h.user_id) AS reproducoes
@@ -8,7 +9,6 @@ INNER JOIN
 INNER JOIN `SpotifyClone`.users AS u
 ON t.track_id = h.track_id
 AND h.user_id = u.user_id
+WHERE u.plan_id = 1 OR u.plan_id = 3
 GROUP BY t.track_name
 ORDER BY t.track_name;
-
-SELECT * FROM `SpotifyClone`.subscription_plans;
